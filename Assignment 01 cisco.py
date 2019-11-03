32a75564f3411da980798d383044d56c5b65f93f
SarimAliKhan <45090042+SarimAliKhan@users.noreply.github.com>
2019-11-03 20:36:15 +0500

    Add files via upload

---
+++ Assignment 01 cisco.py
@@ -0,0 +1,24 @@
+#Q1
+print('Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, Twinkle, little star,\n\tHow I wonder what you are')
+
+#Q2
+import sys
+print(sys.version)
+
+#Q3
+import datetime
+print(datetime.datetime.today())
+
+#Q4
+input1 = int(input("enter the radius of circle"))
+print("area of circle is : ",2*3.142*input1)
+
+#Q5
+f_name = input("First name : ")
+l_name = input("Last name : ")
+print(l_name + ' ' + f_name)
+
+#Q6
+n1 = int(input("input number 01 :"))
+n2 = int(input("input number 02 :"))
+print(n1+n2)
